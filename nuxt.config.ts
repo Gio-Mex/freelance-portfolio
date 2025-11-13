a66f81587ecfaafe5d3a012583b28af95a1cd374
Giorgio Messore <giorgio.messore@live.it>
2025-11-13 16:59:07 +0100

    little update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  nitro: {
+    preset: "vercel-static",
+  },
   css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
