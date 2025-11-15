dcd8992ec315f9c0e81b43870b79f476089f09c3
Giorgio Messore <giorgio.messore@live.it>
2025-11-16 00:16:03 +0100

    imgs updated

--- .vercel/output/functions/__fallback.func/chunks/build/CanvasParticles-styles.DukBjktU.mjs
+++
@@ -1,8 +0,0 @@
-const CanvasParticles_vue_vue_type_style_index_0_scoped_c0029f9d_lang = "canvas[data-v-c0029f9d]{pointer-events:none}";
-
-const CanvasParticlesStyles_DukBjktU = [
-  CanvasParticles_vue_vue_type_style_index_0_scoped_c0029f9d_lang
-];
-
-export { CanvasParticlesStyles_DukBjktU as default };
-//# sourceMappingURL=CanvasParticles-styles.DukBjktU.mjs.map
--- .vercel/output/functions/__fallback.func/chunks/build/bio-styles.BKK1JfmO.mjs
+++
@@ -1,8 +0,0 @@
-const bio_vue_vue_type_style_index_0_scoped_21a9ee20_lang = "@keyframes fade-slide-in-21a9ee20{0%{transform:translate(0)}to{opacity:.9;transform:translate(2.5rem)}}.animate-fade-slide-in[data-v-21a9ee20]{animation:fade-slide-in-21a9ee20 1s ease-out .8s forwards}.perspective[data-v-21a9ee20]{perspective:1000px}.preserve-3d[data-v-21a9ee20]{transform-style:preserve-3d}.group-hover\\:rotate-y-180[data-v-21a9ee20]:hover,.rotate-y-180[data-v-21a9ee20]{transform:rotateY(180deg)}";
-
-const bioStyles_BKK1JfmO = [
-  bio_vue_vue_type_style_index_0_scoped_21a9ee20_lang
-];
-
-export { bioStyles_BKK1JfmO as default };
-//# sourceMappingURL=bio-styles.BKK1JfmO.mjs.map
